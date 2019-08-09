9877087cf859eb0f4e01c955107417cc8511729a
Rajat Sudagade <rajat.k16@gmail.com>
2019-08-09 00:19:03 -0500

    Project Setup

---
+++ src/pages/homepage/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Directory from '../../components/Directory';
+import './style.css';
+
+const HomePage = () => {
+  return (
+    <div className="homepage">
+      <Directory/>
+    </div>
+  )
+}
+
+export default HomePage;
