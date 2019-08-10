acf7a719c327ae8a8c91889ebdf3d2a9c561643e
Rajat Sudagade <rajat.k16@gmail.com>
2019-08-10 01:25:44 -0500

    Firebase SignIn with Email and Password

--- src/components/SignIn/index.jsx
+++ src/components/SignIn/index.jsx
@@ -2,7 +2,7 @@ import React, {Component} from 'react'
 
 import FormInput from '../FormInput';
 import CustomButton from '../CustomButton';
-import {signInWithGoogle} from '../../firebase/firebase.utils';
+import {auth, signInWithGoogle} from '../../firebase/firebase.utils';
 import './style.css';
 
 class SignIn extends Component {
@@ -11,12 +11,17 @@ class SignIn extends Component {
     password: ''
   }
 
-  handleSubmit = (event) => {
+  handleSubmit = async event => {
     event.preventDefault();
-    this.setState({
-      email: '',
-      password: ''
-    })
+    
+    const {email, password} = this.state;
+    try {
+      await auth.signInWithEmailAndPassword(email, password);
+      this.setState({email: '',password: ''})
+    } catch (error) {
+      console.log(error)
+    }
+
   }
 
   handleChange = event => {
