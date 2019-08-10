908b4b5a4de33798de3934342184c3b87136cc7b
Rajat Sudagade <rajat.k16@gmail.com>
2019-08-10 15:12:54 -0500

    Added Redux for State Management

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
-import {Link} from 'react-router-dom'
-import {ReactComponent as Logo} from '../../assets/crown.svg';
-import {auth} from '../../firebase/firebase.utils';
+import React from 'react';
+import {Link} from 'react-router-dom';
+import {connect} from 'react-redux';
+
 import './style.css';
+import {auth} from '../../firebase/firebase.utils';
+import {ReactComponent as Logo} from '../../assets/crown.svg';
 
 const Header = ({currentUser}) => {
   return (
@@ -32,4 +34,8 @@ const Header = ({currentUser}) => {
   )
 }
 
-export default Header;
+const mapStateToProps = (state) => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStateToProps)(Header);
